77894d3e1e51a5ee70f005435069513d0163fc05
Karteek <saikarteek.1988@gmail.com>
2020-01-22 19:43:17 +0530

    initial project setup for JWT Web Application

---
+++ jwtwebtoken/src/main/java/com/learning/jwtwebtoken/JwtwebtokenApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jwtwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtwebtokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtwebtokenApplication.class, args);
+	}
+
+}
---
+++ jwtwebtoken/src/test/java/com/learning/jwtwebtoken/JwtwebtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jwtwebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtwebtokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
